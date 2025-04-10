fcce777e483c5db67a4cdfd396983072d1a885a9
Lucas Eduardo <lucaseduardo3528@hotmail.com>
2025-04-10 18:29:51 -0300

    feature: implementing users card

--- innyx-frontend/src/components/users/UserCard.vue
+++ innyx-frontend/src/components/users/UserCard.vue
@@ -2,17 +2,21 @@
 import type { User } from '@/types/user'
 const props = defineProps<{ user: User }>()
 const emit = defineEmits(['edit', 'delete'])
+
+console.log(props.user);
 </script>
 
 <template>
-    <div class="border rounded p-4 shadow-sm bg-white dark:bg-quaternary-800">
+    <div class="border rounded-xl p-4 shadow-sm bg-tertiary">
         <div class="font-semibold">{{ user.name }}</div>
         <div class="text-sm text-gray-600">{{ user.email }}</div>
         <div class="text-sm text-gray-500">Perfil: {{ user.role_name }}</div>
 
         <div class="flex gap-2 mt-4">
-            <button @click="$emit('edit', user)" class="text-blue-500 hover:underline">Editar</button>
-            <button @click="$emit('delete', user)" class="text-red-500 hover:underline">Excluir</button>
+            <button @click="$emit('edit', user)"
+                class="text-white hover:bg-purple-600 w-24 bg-primary rounded-md">Editar</button>
+            <button @click="$emit('delete', user)"
+                class="bg-red-700 hover:bg-red-500 w-24 rounded-md text-white">Excluir</button>
         </div>
     </div>
 </template>
